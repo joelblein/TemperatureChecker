5255e29ee3165ef9eef77746a6dd3004a86ff395
joel <joel@joel-NM871AA-ABF-s3825fr>
2015-05-21 23:11:49 +0200

    Communication XML over MQTT ok

--- src/com/job/temperatureapp/MessageReceiver.java
+++ src/com/job/temperatureapp/MessageReceiver.java
@@ -1,35 +1,90 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.job.temperatureapp;
 
+import com.job.probemanager.ProbeEncoder;
+import com.job.probemanager.ProbeEvent;
+import com.job.probemanager.ProbeFailedEvent;
+import com.job.probemanager.ProbeMqttLogger;
+import com.job.probemanager.ProbeReadEvent;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.bind.JAXBException;
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
+
 /**
- *
  * @author joel
- */
+ */ 
 public class MessageReceiver implements MqttCallback {
 
+    String serverURI;
+    String clientId;
+    ProbeEncoder encoder;
+    MqttClient mqttClient = null;
+
+    public MessageReceiver(String serverURI, String clientId, ProbeEncoder encoder) {
+        this.serverURI = serverURI;
+        this.clientId = clientId;
+        this.encoder = encoder;
+    }
+            
+    synchronized protected MqttClient getMqttClient() throws MqttException {
+        if(mqttClient == null) {
+            try  {
+                    System.out.println("Trying to connect MQTT server at "+serverURI+" with clientId "+clientId);
+                    mqttClient = new MqttClient(serverURI, clientId);
+                    mqttClient.setCallback(this);
+                    mqttClient.connect();
+                    System.out.println("Connected to mqtt server");
+            } catch (IllegalArgumentException iae) {
+                System.out.println("Unable to connect MQTT server : Invalid parameters (serverURI="+serverURI+", clientId="+clientId+")"); 
+                iae.printStackTrace();
+            }    
+        }
+        return mqttClient;
+    }
+
+    public synchronized void start() {
+        try {
+            getMqttClient();
+            mqttClient.subscribe("cafetiton/temperature");
+        } catch (MqttException me) {
+            System.out.println("Unable to start MQTT server : "+me); 
+            me.printStackTrace();
+        }    
+    }
+
+    public synchronized void stop() {
+        try  {
+            if(mqttClient!=null) {
+                mqttClient.disconnect();
+            }
+        } catch (MqttException me) {
+            System.out.println("Unable to stop MQTT server : "+me); 
+            me.printStackTrace();
+        } finally {
+            mqttClient = null;
+        }
+    }
+
     @Override
-    public void connectionLost(Throwable thrwbl) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public synchronized void connectionLost(Throwable thrwbl) {
+        System.out.println("Connection to mqtt server lost :"+thrwbl);
+        mqttClient = null;
+        start();
     }
 
     @Override
     public void messageArrived(String topic, MqttMessage mm) throws Exception {
-        System.out.println("Received message for topic :"+topic);
-        System.out.println("Message is :"+new String(mm.getPayload()));
-
+        System.out.println("Message reçu : "+new String(mm.getPayload()));
+        System.out.println(encoder.decode(mm.getPayload()));
     }
 
     @Override
     public void deliveryComplete(IMqttDeliveryToken imdt) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
--- src/com/job/temperatureapp/TemperatureApp.java
+++ src/com/job/temperatureapp/TemperatureApp.java
@@ -5,6 +5,7 @@
  */
 package com.job.temperatureapp;
 
+import com.job.probemanager.XmlEncoder;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import org.eclipse.paho.client.mqttv3.MqttClient;
@@ -20,34 +21,24 @@ public class TemperatureApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        if(args.length<1) {
-            throw new RuntimeException("Needs MQTTBroker ip as an argument");
+        if(args.length<2) {
+            throw new RuntimeException("Needs MQTTBroker brokeruri and clientid as arguments");
         }
         String mqttBroketIP = args[0];
-
-        String hostname = "Unknown";
-        try {
-            InetAddress addr;
-            addr = InetAddress.getLocalHost();
-            hostname = addr.getHostName();
-        } catch (UnknownHostException ex) {
-            System.out.println("Hostname can not be resolved");
-        }
-
+        String hostname = args[1];
+        
+        MessageReceiver messageReceiver = new MessageReceiver(mqttBroketIP, hostname, new XmlEncoder());
         try  {
-            MqttClient mqttClient = new MqttClient(mqttBroketIP, hostname);
-            mqttClient.setCallback(new MessageReceiver());
-            mqttClient.connect();    
-            mqttClient.subscribe("cafetiton/temperature");
-            mqttClient.disconnect();
-        } catch (IllegalArgumentException iae) {
-            System.out.println("Invalid parameters to connect MQTT server (serverURI="+mqttBroketIP+", clientId="+hostname+")"); 
-            iae.printStackTrace();
-        } catch (MqttException me) {
-            System.out.println("Unable to connect MQTT server (serverURI="+mqttBroketIP+", clientId="+hostname+")"); 
-            me.printStackTrace();
-        }
+            messageReceiver.start();
 
+            while(true)  // Endors le thread principal (le timer prend la suite)
+                try {
+                    Thread.sleep(60*60*1000);
+                } catch(InterruptedException e) {
+                } 
+        } finally {
+            messageReceiver.stop(); // se délogue du serveur mqtt
+        }
     }
-    
 }
+    
